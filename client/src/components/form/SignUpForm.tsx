5b665af6f7417f1c584c5308243945071348015e
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-02-14 19:45:16 +0500

    added signup Form

---
+++ client/src/components/form/SignUpForm.tsx
@@ -0,0 +1,146 @@
+"use client";
+
+import Link from "next/link";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { useToast } from "@/components/ui/use-toast"
+
+import GoogleSignInButton from "../GoogleSignInButton";
+import { signUpFormSchema } from "../../schema/zod";
+
+const SignUpForm = () => {
+  const form = useForm<z.infer<typeof signUpFormSchema>>({
+    resolver: zodResolver(signUpFormSchema),
+    defaultValues: {
+      username: "naveed ali",
+      email: "naveedalirehmani123@gmail.com",
+      password: "passpass",
+      confirmPassword: "passpass",
+    },
+  });
+  const { toast } = useToast()
+
+  const onSubmit = async (values: z.infer<typeof signUpFormSchema>) => {
+    console.log(values);
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/v1/authentication/local/signup",
+        {
+          ...values,
+        }
+      );
+      console.log(response, "signupResponse");
+      console.log('1')
+      toast({
+        variant:'success',
+        description: "Sign Up Success!",
+      })
+    } catch (error) {
+      console.log(error, "signupResponseError");
+      console.log('12')
+      toast({
+        variant: "destructive",
+        description: error.response.data!,
+      })
+    }
+  };
+
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <div className="space-y-2">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="johndoe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="mail@example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Re-Enter your password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Re-Enter your password"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button className="w-full mt-6 dark:bg-slate-100" type="submit">
+          Sign up
+        </Button>
+      </form>
+      <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+        or
+      </div>
+      <GoogleSignInButton>Sign up with Google</GoogleSignInButton>
+      <p className="text-center text-sm text-gray-600 mt-2">
+        If you don&apos;t have an account, please&nbsp;
+        <Link className="text-blue-500 hover:underline" href="/sign-in">
+          Sign in
+        </Link>
+      </p>
+    </Form>
+  );
+};
+
+export default SignUpForm;
